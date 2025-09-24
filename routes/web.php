a2fbace5f3a10a0e8c2283e5a844b1278fed743c
Nextraile Vestrune <nextraile@gmail.com>
2025-09-24 14:11:17 +0700

    refactor: update routes to use controller methods

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+use App\Http\Controllers\LandingController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\VisionsController;
+use App\Http\Controllers\SubVisionsController;
+use App\Http\Controllers\TagsController;
+use App\Http\Controllers\TaggablesController;
+
+Route::get('/', [LandingController::class, 'index']);
+Route::prefix('user')->group( function () {
+    Route::get('/dashboard', [UserController::class, 'index']);
+    Route::get('/settings', [UserController::class, 'preferences']);
+    Route::get('/visions', [VisionsController::class, 'index']);
+    Route::get('/tags', [TagsController::class, 'index']);
 });
